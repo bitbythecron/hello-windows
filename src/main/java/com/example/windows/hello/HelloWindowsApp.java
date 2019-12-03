0b480b2afcf831845bf461c88ca79145211ad86d
Zac Harvey <bitbythecron@gmail.com>
2019-12-03 11:57:52 -0500

    Adding POI functionality

--- src/main/java/com/example/windows/hello/HelloWindowsApp.java
+++ src/main/java/com/example/windows/hello/HelloWindowsApp.java
@@ -1,15 +1,18 @@
 package com.example.windows.hello;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 import com.jeta.forms.components.panel.FormPanel;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.UUID;
 
 @Slf4j
@@ -60,7 +63,61 @@ public class HelloWindowsApp {
             goButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    nameTextField.setText(UUID.randomUUID().toString());
+                    String uuid = UUID.randomUUID().toString();
+                    nameTextField.setText(uuid);
+
+                    Workbook workbook = new XSSFWorkbook();
+                    Sheet sheet = workbook.createSheet("SOME_SHEET");
+
+                    Font headerFont = workbook.createFont();
+                    headerFont.setBold(true);
+
+                    CellStyle cellStyle = workbook.createCellStyle();
+                    cellStyle.setFont(headerFont);
+                    cellStyle.setFillBackgroundColor(IndexedColors.YELLOW.getIndex());
+                    cellStyle.setAlignment(HorizontalAlignment.CENTER);
+
+                    int rowNum = 0;
+
+                    Row headerRow = sheet.createRow(rowNum);
+                    headerRow.setRowStyle(cellStyle);
+
+                    Cell partNumberHeaderCell = headerRow.createCell(0);
+                    partNumberHeaderCell.setCellValue("Part #");
+                    partNumberHeaderCell.setCellStyle(cellStyle);
+
+                    Cell partDescriptionHeaderCell = headerRow.createCell(1);
+                    partDescriptionHeaderCell.setCellStyle(cellStyle);
+                    partDescriptionHeaderCell.setCellValue("Description");
+
+                    Cell partPriceHeaderCell = headerRow.createCell(2);
+                    partPriceHeaderCell.setCellStyle(cellStyle);
+                    partPriceHeaderCell.setCellValue("Price");
+
+                    Cell manufacturerHeaderCell = headerRow.createCell(3);
+                    manufacturerHeaderCell.setCellStyle(cellStyle);
+                    manufacturerHeaderCell.setCellValue("Make");
+
+                    rowNum++;
+
+                    Row nextRow = sheet.createRow(rowNum);
+
+                    nextRow.createCell(0).setCellValue(uuid);
+                    nextRow.createCell(1).setCellValue("Some Part");
+                    nextRow.createCell(2).setCellValue(2.99);
+                    nextRow.createCell(3).setCellValue("ACME");
+
+                    FileOutputStream fos = null;
+                    try {
+                        fos = new FileOutputStream("acme.xlsx");
+
+                        workbook.write(fos);
+
+                        workbook.close();
+                    } catch (IOException ex) {
+                        log.error(ExceptionUtils.getStackTrace(ex));
+                    }
+
                 }
             });
 
